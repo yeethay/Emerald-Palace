bd71d41e98e606270d507e49a4d844a0cece941c
Tai Lai <ctlai95@gmail.com>
2020-05-23 01:47:10 -0600

    Noto serif, parse home page fields from json file

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import home from './home.json';
 import './Home.css';
 
 const Home = () => (
   <div className="main">
     <div className="center">
-      <span className="name">EMERALD PALACE</span>
+      <span className="name">{home.name_zh}</span>
+      <span className="name">{home.name.toUpperCase()}</span>
       <div className="contact-info">
-        <span>55 CASTLERIDGE BLVD NE, CALGARY, AB T3J 3J8, CANADA</span>
-        <span>+1 (403) 568-2832</span>
+        <span>{home.address.toUpperCase()}</span>
+        <span>{home.phone}</span>
       </div>
     </div>
   </div>
