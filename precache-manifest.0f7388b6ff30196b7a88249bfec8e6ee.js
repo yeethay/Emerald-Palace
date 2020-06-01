729708aa76622aed8cd199e4312499758ac672c5
henrytran28 <henrytrann28@gmail.com>
2020-05-31 20:53:49 -0600

    Updates

---
+++ precache-manifest.0f7388b6ff30196b7a88249bfec8e6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c86fb611a4c0c5681d95b6514854416",
+    "url": "/emerald-palace/index.html"
+  },
+  {
+    "revision": "1618dd9a1864926d231c",
+    "url": "/emerald-palace/static/css/main.c10e873c.chunk.css"
+  },
+  {
+    "revision": "17250c315791be1b0461",
+    "url": "/emerald-palace/static/js/2.0d5f4d77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emerald-palace/static/js/2.0d5f4d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1618dd9a1864926d231c",
+    "url": "/emerald-palace/static/js/main.6ff7706b.chunk.js"
+  },
+  {
+    "revision": "ab3e5d37dcdca5ad2df3",
+    "url": "/emerald-palace/static/js/runtime-main.b4613e28.js"
+  }
+]);
