51afb0d96b7ca81474803b7badc58267350ec26b
henrytran28 <henrytrann28@gmail.com>
2020-06-07 21:34:27 -0600

    Put in hamburger, mess around with navbar with media queries

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import navbar from './navbar.json';
 import './Navbar.css';
 
 const Navbar = () => {
   const location = useLocation();
   const [transparent, setTransparent] = useState(true);
-  useEffect(() => {
-    const path = location.pathname;
-    if (path === '/') setTransparent(true);
-    else setTransparent(false);
-  }, [location]);
+  let wrapperRef = useRef<HTMLDivElement>(null);
+
+  // useEffect(() => {
+  //   const path = location.pathname;
+  //   if (path === '/') setTransparent(true);
+  //   else setTransparent(false);
+  // }, [location]);
+
+  const handleClick = () => {
+    const wrapper = wrapperRef.current;
+    wrapper?.classList.toggle('is-nav-open');
+  };
+
   return (
-    <div className={`navbar ${transparent ? 'transparent' : ''}`}>
-      <Link to="/">
-        <div className="nav-item">HOME</div>
-      </Link>
-      <Link to="about">
-        <div className="nav-item">ABOUT</div>
-      </Link>
-      <Link to="menu">
-        <div className="nav-item">MENU</div>
-      </Link>
-      <Link to="takeout-menu">
-        <div className="nav-item">T/O MENU</div>
-      </Link>
-      <Link to="contact">
-        <div className="nav-item">CONTACT</div>
-      </Link>
+    <div ref={wrapperRef} className="wrapper">
+      <div className="navbar">
+        <button className="hamburger" onClick={handleClick}>
+          <FontAwesomeIcon icon={faBars} size="2x" />
+        </button>
+        {navbar.map((item) => (
+          <Link to={item.route}>
+            <div className="nav-item">{item.name}</div>
+          </Link>
+        ))}
+      </div>
+      <div className="mobile-navbar">
+        {navbar.map((item) => (
+          <Link to={item.route}>
+            <div className="">{item.name}</div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
