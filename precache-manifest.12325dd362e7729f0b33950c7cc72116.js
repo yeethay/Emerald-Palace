6a5bb60e86ee2941b4ff7c343f7b26c82280856e
henrytran28 <henrytran28@users.noreply.github.com>
2020-07-22 04:46:36 +0000

    Updates

---
+++ precache-manifest.12325dd362e7729f0b33950c7cc72116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7099ce0a4a3778766af0bd4fb50fdcf",
+    "url": "/Emerald-Palace/index.html"
+  },
+  {
+    "revision": "174afd65b62182ab521f",
+    "url": "/Emerald-Palace/static/css/main.b6e29241.chunk.css"
+  },
+  {
+    "revision": "bef8c92d7dfc089ff260",
+    "url": "/Emerald-Palace/static/js/2.5efaddbb.chunk.js"
+  },
+  {
+    "revision": "d8406a4e3c71e244536f2f13a315837e",
+    "url": "/Emerald-Palace/static/js/2.5efaddbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174afd65b62182ab521f",
+    "url": "/Emerald-Palace/static/js/main.8933d179.chunk.js"
+  },
+  {
+    "revision": "bf3bcd4d8e811edd34c3",
+    "url": "/Emerald-Palace/static/js/runtime-main.7cc3be6f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Emerald-Palace/precache-manifest.cdf8a8e58657b2600b13da8a19dc1251.js"
+  "/Emerald-Palace/precache-manifest.12325dd362e7729f0b33950c7cc72116.js"
 );
 
 self.addEventListener('message', (event) => {
